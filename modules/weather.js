82596064f4369c5e4f5053b7db5d73a2a6bec30a
Konstantin Protzen <code@kondev.de>
2024-05-24 17:37:48 +0200

    ✨ Add cache handling

--- modules/weather.js
+++ modules/weather.js
@@ -1,6 +1,7 @@
 const config = require("../config.json");
 const log = require("../log.js");
 var axios = require("axios").default;
+const fs = require("fs");
 
 // Set up API options
 var api_options = {
@@ -53,8 +54,32 @@ getWeather = () => {
     });
 }
 
+cachedIcons = {};
+/**
+ * Cache a weather icon from the OpenWeatherMap API
+ * @param {string} icon The icon name to cache
+ * @param {function} cb The callback function to call when the icon is cached
+ */
+iconCacheHandler = (icon, cb) => {
+    if (cachedIcons.hasOwnProperty(icon) && Date.now() - cachedIcons[icon].time < config.weather.icons.cacheTime * 1000) {
+        cb();
+        return;
+    }
+    log.log("Fetching weather-icon " + icon + " from OpenWeatherMap", 0);
+    axios.get("https://openweathermap.org/img/wn/" + icon + "@" + config.weather.icons.size + ".png", { responseType: "arraybuffer" }).then((response) => {
+        fs.writeFileSync("./res/weather-icons/" + icon + ".png", response.data);
+        cachedIcons[icon] = { time: Date.now() };
+        cb();
+    });
+}
+
 // Initialize the weather module
 log.log("Initializing weather module", 0);
-getWeather().then(() => log.log("Weather module initialized", 3)).catch((error) => log.log("Error initializing weather module: " + error, 2));
+getWeather().then(() => log.log("Received initial weather state", 3)).catch((error) => log.log("Error initializing weather module: " + error, 2));
+if (!fs.existsSync("./res/weather-icons")) {
+    log.log("Creating weather-icon cache-directory", 0);
+    fs.mkdirSync("./res/weather-icons");
+}
+if (config.weather.icons.cache) config.weather.icons.precache.forEach((icon) => iconCacheHandler(icon, () => { }));
 
-module.exports = { getWeather };
+module.exports = { getWeather, iconCacheHandler };
